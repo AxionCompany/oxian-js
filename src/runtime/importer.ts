94afb4dc6e4795a9a540014fa09e3f86d7de0a0f
Vinicius Santos <vfssantos@outlook.com>
2025-08-11 23:04:25 -0300

    adding source map

--- src/runtime/importer.ts
+++ src/runtime/importer.ts
@@ -5,6 +5,6 @@ import { encodeBase64 } from "@std/encoding/base64";
 export async function importModule(url: URL, loaders: Loader[], ttlMs = 60_000, projectRoot?: string): Promise<Record<string, unknown>> {
     const code = await bundleModule(url, loaders, ttlMs, projectRoot);
     const codeWithSourceUrl = `${code}\n//# sourceURL=${url.toString()}`;
-    const dataUrl = `data:application/javascript;base64,${encodeBase64(new TextEncoder().encode(codeWithSourceUrl))}`;
+    const dataUrl = `data:application/typescript;base64,${encodeBase64(new TextEncoder().encode(codeWithSourceUrl))}`;
     return await import(dataUrl);
 } 
